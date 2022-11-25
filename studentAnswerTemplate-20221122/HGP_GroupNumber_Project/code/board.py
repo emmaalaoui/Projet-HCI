9bbfb0d630cf682db99d4c16b953b3cb1b234730
user <emmaaoui24@gmail.com>
2022-11-25 18:42:05 +0000

    test

--- studentAnswerTemplate-20221122/HGP_GroupNumber_Project/code/board.py
+++ studentAnswerTemplate-20221122/HGP_GroupNumber_Project/code/board.py
@@ -1,16 +1,17 @@
 from PyQt6.QtWidgets import QFrame
 from PyQt6.QtCore import Qt, QBasicTimer, pyqtSignal, QPointF, QPoint
-from PyQt6.QtGui import QPainter, QPixmap
+from PyQt6.QtGui import QPainter, QPixmap, QPen
 from PyQt6.QtTest import QTest
 from piece import Piece
+from score_board import ScoreBoard
 
 class Board(QFrame):  # base the board on a QFrame widget
     updateTimerSignal = pyqtSignal(int) # signal sent when timer is updated
     clickLocationSignal = pyqtSignal(str) # signal sent when there is a new click location
 
     # TODO set the board width and height to be square
-    boardWidth  = 100     # board is 0 squares wide # TODO this needs updating
-    boardHeight = 100     #
+    boardWidth  = 50     # board is 0 squares wide # TODO this needs updating
+    boardHeight = 50     #
     timerSpeed  = 1000     # the timer updates every 1 millisecond
     counter     = 60    # the number the counter will count down from
 
@@ -19,6 +20,8 @@ class Board(QFrame):  # base the board on a QFrame widget
         super().__init__(parent)
         self.initBoard()
         self.image = QPixmap("./icons/Board.png")
+
+
     def resizeEvent(self, event):
         self.image = self.image.scaled(self.width(), self.height())
 
@@ -27,9 +30,67 @@ class Board(QFrame):  # base the board on a QFrame widget
         self.timer = QBasicTimer()  # create a timer for the game
         self.isStarted = False      # game is not currently started
         self.start()                # start the game which will start the timer
+        #0 représente une case vide, 1 représente les noirs (ce joueur commence) et 2 représente les blancs
+        self.a = 0
+        self.b = 0
+        self.c = 0
+        self.d = 0
+        self.e = 0
+        self.f = 0
+        self.g = 0
+        self.h = 0
+        self.i = 0
+        self.j = 0
+        self.k = 0
+        self.l = 0
+        self.m = 0
+        self.n = 0
+        self.o = 0
+        self.p = 0
+        self.q = 0
+        self.r = 0
+        self.s = 0
+        self.t = 0
+        self.u = 0
+        self.v = 0
+        self.w = 0
+        self.x = 0
+        self.y = 0
+        self.z = 0
+        self.ab = 0
+        self.ac = 0
+        self.ad = 0
+        self.ae = 0
+        self.af = 0
+        self.ag = 0
+        self.ah = 0
+        self.ai = 0
+        self.aj = 0
+        self.ak = 0
+        self.al = 0
+        self.am = 0
+        self.an = 0
+        self.ao = 0
+        self.ap = 0
+        self.aq = 0
+        self.ar = 0
+        self.ay = 0
+        self.at = 0
+        self.au = 0
+        self.av = 0
+        self.aw = 0
+        self.ax = 0
+        #il y'a 49 intersections donc on doit créer un tableau avc 49 cases et le remplir de 0, 1 ou 2
+        '''[self.a, self.b, self.c, self.d, self.e, self.f, self.g],
+          [self.h, self.i, self.j, self.k, self.l, self.m, self.n],
+          [self.o, self.p, self.q, self.r, self.s, self.t, self.u],
+          [self.v, self.w, self.x, self.y, self.z, self.ab, self.ac],
+          [self.ad, self.ae, self.af, self.ag, self.ah, self.ai, self.aj],
+          [self.ak, self.al, self.am, self.an, self.ao, self.ap, self.aq],
+          [self.ar, self.ay, self.at, self.au, self.av, self.aw, self.ax],'''
 
-        self.boardArray =[]         # TODO - create a 2d int/Piece array to store the state of the game
-        # self.printBoardArray()    # TODO - uncomment this method after creating the array above
+        self.boardArray = []        # TODO - create a 2d int/Piece array to store the state of the game
+        self.printBoardArray()    # TODO - uncomment this method after creating the array above
 
     def printBoardArray(self):
         '''prints the boardArray in an attractive way'''
@@ -53,13 +114,17 @@ class Board(QFrame):  # base the board on a QFrame widget
         self.resetGame()                            # reset the game
         self.timer.start(self.timerSpeed, self)     # start the timer with the correct speed
         print("start () - timer is started")
+        '''ScoreBoard.pbar.setValue(0)
+        ScoreBoard.step = ScoreBoard.step + 1
+        ScoreBoard.pbar.setValue(ScoreBoard.step)'''
 
     def timerEvent(self, event):
         '''this event is automatically called when the timer is updated. based on the timerSpeed variable '''
         # TODO adapt this code to handle your timers
         if event.timerId() == self.timer.timerId():  # if the timer that has 'ticked' is the one in this class
-            if Board.counter == 0:
+            if self.counter == 1:
                 print("Game over")
+                self.timer.stop()
             self.counter -= 1
             print('timerEvent()', self.counter)
             self.updateTimerSignal.emit(self.counter)
@@ -71,7 +136,7 @@ class Board(QFrame):  # base the board on a QFrame widget
         '''paints the board and the pieces of the game'''
         painter = QPainter(self)
         painter.drawPixmap(QPoint(), self.image)
-        #self.drawBoardSquares(painter)
+        #self.drawBoardSquares(painter) #we don't draw the board bc we use a background
         self.drawPieces(painter)
 
     def mousePressEvent(self, event):
@@ -84,9 +149,21 @@ class Board(QFrame):  # base the board on a QFrame widget
     def resetGame(self):
         '''clears pieces from the board'''
         # TODO write code to reset game
+        '''
+        self.image = QPixmap("./icons/Board.png")
+        width = self.width()  # get the width of the current QImage in your application
+        height = self.height()  # get the height of the current QImage in your application
+        self.image = self.image.scaled(width, height)
+        '''
 
     def tryMove(self, newX, newY):
         '''tries to move a piece'''
+        '''
+        Equation d'un cercle : (x−h)²+(y−k)²=r².
+        Si newX et newY vérifie l'équation alors le point est dans la zone
+        h : commence à 146 puis on ajoute 90 à chaque fois qu'on se décale sur la droite
+        y : commence à 176 puis on ajoute 110 à chaque fois qu'on se décale vers le bas
+        '''
 
     def drawBoardSquares(self, painter):
         '''draw all the square on the board'''
@@ -106,8 +183,15 @@ class Board(QFrame):  # base the board on a QFrame widget
                 # TODO change the colour of the brush so that a checkered board is drawn
 
     def drawPieces(self, painter):
-        '''draw the prices on the board'''
-        colour = Qt.GlobalColor.transparent # empty square could be modeled with transparent pieces
+        '''draw the pieces on the board'''
+        self.brushSize = 3
+        self.brushColor = Qt.GlobalColor.black
+        #painter = QPainter(self.image)
+        painter.setPen(QPen(self.brushColor, self.brushSize))
+        radius = self.squareWidth() / 4
+        center = QPointF(radius, radius)
+        painter.drawEllipse(center, radius, radius)
+        '''colour = Qt.GlobalColor.transparent # empty square could be modeled with transparent pieces
         for row in range(0, len(self.boardArray)):
             for col in range(0, len(self.boardArray[0])):
                 painter.save()
@@ -122,4 +206,4 @@ class Board(QFrame):  # base the board on a QFrame widget
                 radius = self.squareWidth() / 4
                 center = QPointF(radius, radius)
                 painter.drawEllipse(center, radius, radius)
-                painter.restore()
+                painter.restore()'''
--- studentAnswerTemplate-20221122/HGP_GroupNumber_Project/code/score_board.py
+++ studentAnswerTemplate-20221122/HGP_GroupNumber_Project/code/score_board.py
@@ -1,17 +1,25 @@
-from PyQt6.QtWidgets import QDockWidget, QVBoxLayout, QPushButton, QWidget, QLabel #TODO import additional Widget classes as desired
-from PyQt6.QtCore import pyqtSlot
-
+from PyQt6.QtWidgets import QDockWidget, QApplication, QToolBar, QProgressBar, QVBoxLayout, QPushButton, QWidget, QLabel #TODO import additional Widget classes as desired
+from PyQt6.QtCore import pyqtSlot, QSize, Qt
+from PyQt6.QtGui import QIcon, QAction
+#from board import Board
 class ScoreBoard(QDockWidget):
     '''# base the score_board on a QDockWidget'''
 
     def __init__(self):
         super().__init__()
         self.initUI()
+
+
+        #why it doesn't work ?
         '''mainMenu = self.menuBar()  # create a menu bar
         mainMenu.setNativeMenuBar(False)
         fileMenu = mainMenu.addMenu(
             " File")  # add the file menu to the menu bar, the space is required as "File" is reserved in Mac
-        brushSizeMenu = mainMenu.addMenu(" Brush Size")'''
+
+        clearAction = QAction(QIcon("./icons/clear.png"), "Clear", self)  # create a clear action with a png as an icon
+        clearAction.setShortcut("Ctrl+C")  # connect this clear action to a keyboard shortcut
+        fileMenu.addAction(clearAction)  # add this action to the file menu
+        clearAction.triggered.connect(self.clear)'''
 
     def initUI(self):
         '''initiates ScoreBoard UI'''
@@ -21,16 +29,33 @@ class ScoreBoard(QDockWidget):
         #create a widget to hold other widgets
         self.mainWidget = QWidget()
         self.mainLayout = QVBoxLayout()
+        self.mainWidget.setMaximumSize(220, self.height())
+        #self.toolbar = QToolBar("My main toolbar")
+        #self.toolbar.setIconSize(QSize(16, 16))
 
         #create two labels which will be updated by signals
         self.label_clickLocation = QLabel("Click Location: ")
         self.label_timeRemaining = QLabel("Time remaining: ")
+        self.currentTurn = "Player 1"
+        self.playerLabel = QLabel("Current Turn: " + self.currentTurn)
+        self.mainLayout.addWidget(self.playerLabel)
         self.mainWidget.setLayout(self.mainLayout)
-        self.mainLayout.addWidget(self.label_clickLocation)
+        #self.mainLayout.addWidget(self.label_clickLocation)
+        #self.toolbar.addWidget(self.label_clickLocation)
         self.mainLayout.addWidget(self.label_timeRemaining)
+        #self.toolbar.addWidget(self.label_timeRemaining)
+        self.timerButton = QPushButton('One Minute Timer', self)
+        self.timerButton.clicked.connect(self.buttonTimer_clicked)
+        self.pbar = QProgressBar(self)
+        self.step = 0
+        self.pbar.setOrientation(Qt.Orientation.Vertical)
+        self.mainLayout.addWidget(self.timerButton)
+        self.mainLayout.addWidget(self.pbar)
         closeButton = QPushButton('END GAME')
         closeButton.clicked.connect(self.buttonEnd_cliked)
         self.mainLayout.addWidget(closeButton)
+        #self.toolbar.addWidget(closeButton)
+        #self.setWidget(self.toolbar)
         self.setWidget(self.mainWidget)
         self.show()
 
@@ -61,4 +86,10 @@ class ScoreBoard(QDockWidget):
         # Here I made the buttonEnd_clicked method to close the window when a player cliks on the End button
     def buttonEnd_cliked(self):
         self.close()
+        #à retravailler
+
+    def buttonTimer_clicked(self):
+        print("test")
+        #Board.start()
+
 
