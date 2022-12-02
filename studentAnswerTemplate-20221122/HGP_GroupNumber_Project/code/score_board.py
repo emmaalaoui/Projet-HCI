1e4b843233d89c77bd55448fcd27194af232a3e1
user <emmaaoui24@gmail.com>
2022-12-02 13:01:09 +0000

    cc

--- studentAnswerTemplate-20221122/HGP_GroupNumber_Project/code/score_board.py
+++ studentAnswerTemplate-20221122/HGP_GroupNumber_Project/code/score_board.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QApplication, QDockWidget, QGroupBox, QMenuBar, QProgressBar, QVBoxLayout, QPushButton, QWidget, QLabel #TODO import additional Widget classes as desired
+from PyQt6.QtWidgets import QApplication, QDockWidget, QGroupBox, QMenuBar, QProgressBar, QHBoxLayout, QVBoxLayout, QPushButton, QWidget, QLabel #TODO import additional Widget classes as desired
 from PyQt6.QtCore import pyqtSlot, QSize, Qt, QBasicTimer
 from PyQt6.QtGui import QIcon, QAction, QPixmap, QCursor
 from board import Board
@@ -28,57 +28,76 @@ class ScoreBoard(QDockWidget):
         #create a widget to hold other widgets
         self.mainWidgetB = QGroupBox()
         self.mainWidgetB.setStyleSheet("color: white;"
-                        "background-color: black")
-        self.mainWidgetW = QGroupBox()
-        self.mainWidgetW.setStyleSheet("color: white;"
                                        "background-color: black")
+
+        self.mainWidgetW = QGroupBox()
+        self.mainWidgetW.setStyleSheet("color: black;"
+                                       "background-color: white")
         self.mainLayoutB = QVBoxLayout()
         self.mainLayoutW = QVBoxLayout()
 
-        self.mainWidgetB.setMaximumSize(self.width(), 250)
-        self.mainWidgetW.setMaximumSize(self.width(), 250)
+        #self.mainWidgetB.setMaximumSize(self.width(), 250)
+        self.mainWidgetW.setMaximumSize(self.width(), 300)
 
         #create two labels which will be updated by signals
         self.label_clickLocation = QLabel("Click Location: ")
         self.label_timeRemaining = QLabel("Time remaining: ")
         self.currentTurn = "Player 1"
-        self.playerLabel = QLabel("Current Turn: " + self.currentTurn)
-        self.mainLayoutB.addWidget(self.playerLabel)
-        self.mainLayoutW.addWidget(self.playerLabel)
-        self.mainWidgetB.setLayout(self.mainLayoutB)
-        self.mainWidgetW.setLayout(self.mainLayoutW)
+        self.playerLabelB = QLabel("Current Turn: " + self.currentTurn)
+        self.playerLabelW = QLabel("Current Turn: " + self.currentTurn)
+        #self.mainLayoutB.addWidget(self.playerLabelB)
+        self.mainLayoutW.addWidget(self.playerLabelW)
+        self.scoreW = QLabel("Score : ")
+        #self.scoreB = QLabel("Score : ")
+        self.captureW = QLabel("Captures : ")
+        #self.captureB= QLabel("Captures : ")
         #self.mainLayout.addWidget(self.label_clickLocation)
-        self.mainLayoutB.addWidget(self.label_timeRemaining)
+        #self.mainLayoutB.addWidget(self.label_timeRemaining)
         self.mainLayoutW.addWidget(self.label_timeRemaining)
-        self.timerButton = QPushButton('One Minute Timer', self)
-        self.timerButton.setStyleSheet("color: black;"
+        self.mainLayoutW.addWidget(self.scoreW)
+        #self.mainLayoutB.addWidget(self.scoreB)
+        self.mainLayoutW.addWidget(self.captureW)
+        #self.mainLayoutB.addWidget(self.captureB)
+        self.details = QLabel("Match Details :"+"\n"+self.matchDetails())
+        self.mainLayoutW.addWidget(self.details)
+        #self.timerButtonB = QPushButton('One Minute Timer', self)
+        self.timerButtonW = QPushButton('One Minute Timer', self)
+
+        '''self.timerButtonB.setStyleSheet("color: black;"
                                        "background-color: white;"
                                        #"border-style: outset;"
                                        "border-width: 2px;"
                                        "border-radius: 10px;"
                                        #"border-color: beige;"
-                                       "font: bold 14px")
+                                       "font: bold 14px")'''
 
-        self.timerButton.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
-        self.timerButton.clicked.connect(self.buttonTimer_clicked)
+        #self.timerButtonB.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
+        self.timerButtonW.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
+        #self.timerButtonB.clicked.connect(self.buttonTimer_clicked)
+        self.timerButtonW.clicked.connect(self.buttonTimer_clicked)
         self.pbar = QProgressBar(self)
         self.step = 0
         self.timer = QBasicTimer()
         self.pbar.setOrientation(Qt.Orientation.Vertical)
-        self.mainLayoutB.addWidget(self.timerButton)
-        self.mainLayoutW.addWidget(self.timerButton)
-        self.mainLayoutB.addWidget(self.pbar)
+        #self.mainLayoutB.addWidget(self.timerButtonB)
+        self.mainLayoutW.addWidget(self.timerButtonW)
+        #self.mainLayoutB.addWidget(self.pbar)
         self.mainLayoutW.addWidget(self.pbar)
-        skipButton = QPushButton('Skip Turn')
-        skipButton.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
-        skipButton.clicked.connect(self.buttonSkip_clicked)
-        self.mainLayoutB.addWidget(skipButton)
-        self.mainLayoutW.addWidget(skipButton)
+        #skipButtonB = QPushButton('Skip Turn')
+        skipButtonW = QPushButton('Skip Turn')
+        #skipButtonB.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
+        skipButtonW.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
+        #skipButtonB.clicked.connect(self.buttonSkip_clicked)
+        skipButtonW.clicked.connect(self.buttonSkip_clicked)
+        #self.mainLayoutB.addWidget(skipButtonB)
+        self.mainLayoutW.addWidget(skipButtonW)
         closeButton = QPushButton('END GAME')
         closeButton.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
         closeButton.clicked.connect(self.buttonEnd_cliked)
-        self.mainLayoutB.addWidget(closeButton)
+        #self.mainLayoutB.addWidget(closeButton)
         self.mainLayoutW.addWidget(closeButton)
+        # self.mainWidgetB.setLayout(self.mainLayoutB)
+        self.mainWidgetW.setLayout(self.mainLayoutW)
         #self.setWidget(self.mainWidgetB) #commenter pour afficher la box blanche
         self.setWidget(self.mainWidgetW) #commenter pour afficher la box noire
         self.show()
@@ -116,25 +135,29 @@ class ScoreBoard(QDockWidget):
     def buttonTimer_clicked(self):
         if self.timer.isActive():
             self.timer.stop()
-            self.timerButton.setText('Start')
-        elif self.timerButton.text() == 'Time Over':
+            self.timerButtonB.setText('Start')
+            self.timerButtonW.setText('Start')
+        elif self.timerButtonB.text() == 'Time Over' | self.timerButtonW.text() == 'Time Over':
             self.pbar.setValue(0)
             self.step = 0
             # I want that my progress bar updates during 1 minute (to do a 1-minute timer)
             # So I choose to start my timer every 600 milliseconds because 600 milliseconds equal 0.6 seconds
             # My progress bar max is 100 so 0.6*100 = 60 seconds --> 1 minute
             self.timer.start(600, self)
-            self.timerButton.setText('Stop')
+            self.timerButtonB.setText('Stop')
+            self.timerButtonW.setText('Stop')
         else:
             self.timer.start(600, self)
-            self.timerButton.setText('Stop')
+            self.timerButtonB.setText('Stop')
+            self.timerButtonW.setText('Stop')
             board = Board(self)
             board.start()
 
     def timerEvent(self, e):
         if self.step >= 100:
             self.timer.stop()
-            self.timerButton.setText('Time Over')
+            self.timerButtonB.setText('Time Over')
+            self.timerButtonW.setText('Time Over')
 
         self.step = self.step + 1
         self.pbar.setValue(self.step)
@@ -156,15 +179,32 @@ class ScoreBoard(QDockWidget):
         self.pbar.setValue(0)
         self.step = 0
         self.timer.stop()
-        self.timerButton.setText('One Minute Timer')
+        #self.timerButtonB.setText('One Minute Timer')
+        self.timerButtonW.setText('One Minute Timer')
         board = Board(self)
         board.timer.stop() #ne fonctionne pas !
         self.updateUi()
 
+    def matchDetails(self):
+        self.text = ""
+        count = 0
+        #board = Board(self)
+        for i in range(0, 100, 1):
+            count = i
+            self.text = str(count) + "."
+            #if board.tryMove():
+
+
+        self.text += self.currentTurn
+        return self.text
+
+
         # Here I made the updateUI method to update the UI
     def updateUi(self):
-        self.playerLabel.setText("Current Turn: " + self.currentTurn)
-        self.playerLabel.adjustSize()
+        #self.playerLabelB.setText("Current Turn: " + self.currentTurn)
+        #self.playerLabelB.adjustSize()
+        self.playerLabelW.setText("Current Turn: " + self.currentTurn)
+        self.playerLabelW.adjustSize()
 
 
 
