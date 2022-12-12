5dbf84970671ecccd85af7c71b9f23d2992cacf5
yann <yann.030102@gmail.com>
2022-12-12 12:59:47 +0000

    ça MARCHE !!!!!!!!

--- studentAnswerTemplate-20221122/HGP_GroupNumber_Project/code/go.py
+++ studentAnswerTemplate-20221122/HGP_GroupNumber_Project/code/go.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QMainWindow, QHBoxLayout, QWidget,QLayout
+from PyQt6.QtWidgets import QMainWindow, QHBoxLayout, QWidget, QLayout
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QIcon
 from board import Board
@@ -6,10 +6,24 @@ from score_board import ScoreBoard
 from game_logic import GameLogic
 from endGameWindow import EndGameWindow
 
+
 class Go(QMainWindow):
 
     def __init__(self):
         super().__init__()
+        self.widthMinusHeight = 138
+        self.formerWidth = 690
+        self.formerHeight = self.formerWidth - self.widthMinusHeight
+        self.firstResize = True
+        self.widthResizement = False
+        self.heightResizement = False
+        self.con = True
+        self.count = 0
+        print(self.width(), self.height())
+        self.resize(self.formerWidth, self.formerHeight)
+        print(self.width(), self.height())
+        self.setMinimumSize(self.formerWidth - 5, self.formerHeight - 5)
+        self.setMaximumSize(660+138, 660)
         self.initUI()
 
     def getBoard(self):
@@ -24,7 +38,7 @@ class Go(QMainWindow):
         self.scoreBoard = ScoreBoard(self)
         self.board = Board(self)
         self.endGameWindow = EndGameWindow(self)
-        #self.addDockWidget(Qt.DockWidgetArea.RightDockWidgetArea, self.scoreBoard)
+        # self.addDockWidget(Qt.DockWidgetArea.RightDockWidgetArea, self.scoreBoard)
         self.myScoreBoard = QHBoxLayout()
         self.myScoreBoard.addWidget(self.board, 6)
         self.myScoreBoard.addWidget(self.scoreBoard, 1)
@@ -33,8 +47,8 @@ class Go(QMainWindow):
         self.setCentralWidget(mainWidget)
         self.scoreBoard.make_connection(self.board)
 
-        self.resize(850, 850)
-        #self.center()
+
+        # self.center()
         self.setStyleSheet("background-color: light grey")
         self.setWindowTitle("GroupProject - Go - HGP-FT02")
         self.setWindowIcon(
@@ -44,6 +58,31 @@ class Go(QMainWindow):
     def showEndWindow(self):
         self.hide()
         self.endGameWindow.show()
+
+    def resizeEvent(self, event):
+        if not self.firstResize:
+            if self.formerHeight != self.height() and self.formerWidth != self.width():
+                if abs(self.formerHeight - self.height()) < abs(self.formerWidth - self.width()):
+                    self.formerHeight = self.height()
+                else:
+                    self.formerWidth = self.width()
+            if self.formerHeight == self.height():
+                if self.width() - self.height() != self.widthMinusHeight:
+                    self.formerWidth = self.width()
+                    self.resize(self.width(), self.width() - self.widthMinusHeight)
+                    self.count = self.count + 1
+            else:
+                if self.formerWidth == self.width():
+                    if self.width() - self.height() != self.widthMinusHeight:
+                        self.formerHeight = self.height()
+                        self.resize(self.height() + self.widthMinusHeight, self.height())
+                        self.count = self.count + 1
+
+        elif self.firstResize:
+            print(self.firstResize)
+            self.firstResize = False
+
+
     def center(self):
         '''centers the window on the screen'''
         gr = self.frameGeometry()
@@ -51,8 +90,5 @@ class Go(QMainWindow):
 
         gr.moveCenter(screen)
         self.move(gr.topLeft())
-        #size = self.geometry()
-        #self.move((screen.width() - size.width()) / 2,(screen.height() - size.height()) / 2)
-
-
-
+        # size = self.geometry()
+        # self.move((screen.width() - size.width()) / 2,(screen.height() - size.height()) / 2)
